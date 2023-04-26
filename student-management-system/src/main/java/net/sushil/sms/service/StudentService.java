472b735a38360ff44c589b1074a49a3fc90e12e9
Sushil Verma <sushil.bit.cse@gmail.com>
2023-04-26 23:05:00 +0530

    Add files via upload

---
+++ student-management-system/src/main/java/net/sushil/sms/service/StudentService.java
@@ -0,0 +1,17 @@
+package net.sushil.sms.service;
+
+import net.sushil.sms.dto.StudentDto;
+
+import java.util.List;
+
+public interface StudentService {
+    List<StudentDto> getAllStudents();
+
+    void createStudent(StudentDto student);
+
+    StudentDto getStudentById(Long studentId);
+
+    void updateStudent(StudentDto studentDto);
+
+    void deleteStudent(Long studentId);
+}
