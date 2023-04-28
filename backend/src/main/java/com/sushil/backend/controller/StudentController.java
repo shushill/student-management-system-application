ca807f4e1159ad3138adb8531942e8bec2bf362c
shushill <sushil.bit.cse@gmail.com>
2023-04-28 22:02:45 +0530

    delete api configured

--- backend/src/main/java/com/sushil/backend/controller/StudentController.java
+++ backend/src/main/java/com/sushil/backend/controller/StudentController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -55,12 +56,12 @@ public class StudentController {
     //     return "redirect:/students";
     // }
 
-    // // Handler method to handle delete student request
-    // @GetMapping("/students/{studentId}/delete")
-    // public String deleteStudent(@PathVariable("studentId") Long studentId){
-    //     studentService.deleteStudent(studentId);
-    //     return "redirect:/students";
-    // }
+    // Handler method to handle delete student request
+    @DeleteMapping("/students/{studentId}")
+    public ResponseEntity<String> deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+        return new ResponseEntity<>("Successfully deleted student details", HttpStatus.OK);
+    }
 
     // // Handler method to handle view student request
     // @GetMapping("/students/{studentId}/view")
