958c71261f453344b1461755fe4fb8fbe250c5ca
Sushil Verma <sushil.bit.cse@gmail.com>
2023-04-28 12:58:10 +0000

    some files were not configured so deleting it.

--- student-management-system/src/main/java/net/sushil/sms/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package net.sushil.sms.repository;
-
-import net.javaguides.sms.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
